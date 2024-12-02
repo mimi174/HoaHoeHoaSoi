0b24bd942b121d828cf434b55fe7fb7d17916a52
huytg1612 <huytg1612@gmail.com>
2024-12-02 21:17:07 +0700

    change password

--- Webbanhoa/Pages/ChangePass.cshtml.cs
+++ Webbanhoa/Pages/ChangePass.cshtml.cs
@@ -17,27 +17,21 @@ namespace HoaHoeHoaSoi.Pages
         public string Id { get; set; }
 
         [BindProperty]
+        public string Avatar { get; set; }
+
+        [BindProperty]
         [Required]
-        [MaxLength(20)]
+        public string CurrentPassword { get; set; }
+        [BindProperty]
+        [Required]
         [MinLength(5)]
-        public string Fullname { get; set; }
+        [MaxLength(10)]
+        public string NewPassword { get; set; }
+        [BindProperty]
+        [Required]
+        public string VerifiedPassword { get; set; }
         [BindProperty]
         public string Username { get; set; }
-        [BindProperty]
-        public string Address { get; set; }
-        [BindProperty]
-        public string Phone { get; set; }
-        [BindProperty]
-        public string Avatar { get; set; }
-        [BindProperty]
-        public string Email { get; set; }
-        [BindProperty]
-        public bool Gender { get; set; }
-        [BindProperty]
-        public DateTime? DOB { get; set; }
-        [BindProperty]
-        [ImageExtenstion]
-        public IFormFile FileAvatar { get; set; }
         private IEnumerable<ModelErrorCollection> Errors { get; set; }
         public string ErrorMsg { get; set; } = string.Empty;
         public bool IsSuccess = false;
@@ -49,22 +43,16 @@ namespace HoaHoeHoaSoi.Pages
                 using (SqlConnection connection = HoaDBContext.GetSqlConnection())
                 {
                     connection.Open();
-                    string sql = "SELECT * FROM UserInfo where Id=@Id";
+                    string sql = "SELECT Avatar, Username FROM UserInfo where Id=@Id";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
                         command.Parameters.AddWithValue("@Id", Id);
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            while (reader.Read())
+                            if (reader.Read())
                             {
+                                Avatar = reader.GetString(0);
                                 Username = reader.GetString(1);
-                                Fullname = reader.GetString(3);
-                                DOB = reader.IsDBNull(4) ? null : reader.GetDateTime(4);
-                                Address = reader.IsDBNull(5) ? string.Empty : reader.GetString(5);
-                                Phone = reader.IsDBNull(6) ? string.Empty : reader.GetString(6);
-                                Avatar = reader.IsDBNull(7) ? string.Empty : reader.GetString(7);
-                                Gender = reader.IsDBNull(8) ? false : reader.GetBoolean(8);
-                                Email = reader.IsDBNull(9) ? string.Empty : reader.GetString(9);
                             }
                         }
                     }
@@ -77,10 +65,43 @@ namespace HoaHoeHoaSoi.Pages
             }
         }
 
+        private string GetUserPassword()
+        {
+            string result = string.Empty;
+            try
+            {
+                using (SqlConnection connection = HoaDBContext.GetSqlConnection())
+                {
+                    connection.Open();
+                    string sql = "SELECT Password FROM UserInfo where Id=@Id";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", Id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                result = reader.GetString(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            if (string.IsNullOrEmpty(result))
+                throw new Exception("Can't get user password");
+            return result;
+        }
+
         public IActionResult OnPost()
         {
             ModelState.Remove("Avatar");
-            ModelState.Remove("FileAvatar");
+            ModelState.Remove("Username");
             if (!ModelState.IsValid)
             {
                 Errors = ModelState.Where(x => x.Value != null && x.Value.Errors.Count() > 0).Select(x => x.Value.Errors);
@@ -88,12 +109,24 @@ namespace HoaHoeHoaSoi.Pages
                 {
                     ErrorMsg += string.Join("<br/>", error.Where(e => !string.IsNullOrEmpty(e.ErrorMessage)).Select(e => e.ErrorMessage)) + "<br/>";
                 }
-                return Page();
             }
 
-            if (FileAvatar != null)
+            var password = GetUserPassword();
+            if (string.IsNullOrEmpty(ErrorMsg))
             {
-                Avatar = FuncHelpers.ConvertImgToBase64(FileAvatar);
+                if (CurrentPassword != password)
+                {
+                    ErrorMsg = "Current password is wrong";
+                }
+                else if (NewPassword != VerifiedPassword)
+                {
+                    ErrorMsg = "Verify password not match";
+                }
+            }
+
+            if (!string.IsNullOrEmpty(ErrorMsg)) 
+            { 
+                return Page();
             }
 
             try
@@ -102,16 +135,10 @@ namespace HoaHoeHoaSoi.Pages
                 {
                     connection.Open();
 
-                    string sql = "UPDATE UserInfo SET mail=@email,gender=@gender,dob=@dob,address=@address,phone=@phone,name=@name,avatar=@avatar WHERE Id=@id";
+                    string sql = "UPDATE UserInfo SET password=@password WHERE Id=@id";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
-                        command.Parameters.AddWithValue("@email", Email);
-                        command.Parameters.AddWithValue("@Name", Fullname);
-                        command.Parameters.AddWithValue("@gender", Gender ? 1 : 0);
-                        command.Parameters.AddWithValue("@DOB", DOB);
-                        command.Parameters.AddWithValue("@address", Address);
-                        command.Parameters.AddWithValue("@phone", Phone);
-                        command.Parameters.AddWithValue("@avatar", string.IsNullOrEmpty(Avatar) ? "" : Avatar);
+                        command.Parameters.AddWithValue("@password", NewPassword);
                         command.Parameters.AddWithValue("@id", Id);
 
                         command.ExecuteNonQuery();
