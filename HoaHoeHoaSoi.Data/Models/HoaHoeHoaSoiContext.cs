f3c41da0d1a469ea1d6cc92c923af93f973cd991
sarangmiemie@gmail.com <sarangmiemie@gmail.com>
2024-12-23 23:42:54 +0700

    1 xiu css không đáng kể

--- HoaHoeHoaSoi.Data/Models/HoaHoeHoaSoiContext.cs
+++ HoaHoeHoaSoi.Data/Models/HoaHoeHoaSoiContext.cs
@@ -31,7 +31,7 @@ public partial class HoaHoeHoaSoiContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Initial Catalog=HoaHoeHoaSoi;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;");
+        => optionsBuilder.UseSqlServer("Server=(local);Initial Catalog=HoaHoeHoaSoi;User Id=sa;Password=Password.1; Integrated Security=True;Encrypt=True;Trust Server Certificate=True;Trusted_Connection=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
